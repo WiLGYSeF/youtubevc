f86c9e247636a8b2fe24b2275fc181a0c67d824d
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-01-30 14:40:27 -0500

    add header and footer

--- YouTubeVideoControl/src/App.tsx
+++ YouTubeVideoControl/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-
 import { Outlet } from 'react-router-dom';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 import './css/style.min.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hi</p>
-      <Outlet />
+      <Header />
+      <div className="app-contents">
+        <Outlet />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ YouTubeVideoControl/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../css/style.min.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+
+    </div>
+  );
+}
+
+export default Footer;
