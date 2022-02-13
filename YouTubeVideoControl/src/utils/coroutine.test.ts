3fdf8a22c5ba5daaaf77488fe5c1de0ddbbcbfb7
WiLGYSeF <37422580+WiLGYSeF@users.noreply.github.com>
2022-02-13 18:19:20 -0500

    add coroutine tests

---
+++ YouTubeVideoControl/src/utils/coroutine.test.ts
@@ -0,0 +1,118 @@
+import Coroutine from './coroutine';
+
+describe('coroutine', () => {
+  const mockRequestAnimationFrame =
+    () => jest.spyOn(window, 'requestAnimationFrame')
+      .mockImplementation(() => 0);
+
+  it('runs callback', () => {
+    const raf = mockRequestAnimationFrame();
+
+    const start = 0;
+    const total = 3;
+
+    const func = jest.fn();
+    const routine = new Coroutine(func);
+
+    routine.start();
+    for (let time = start; time < total; time += 1) {
+      routine['doCallback'](time);
+    }
+    routine.stop();
+    routine['doCallback'](total);
+
+    expect(func).toBeCalledTimes(total);
+    expect(routine.startTime).toBe(start);
+    expect(routine.lastCallbackTime).toBe(total - 1);
+    expect(routine.callbackCount).toBe(total);
+    raf.mockRestore();
+  });
+
+  it('runs callback to timeout', () => {
+    const raf = mockRequestAnimationFrame();
+
+    const start = 0;
+    const timeout = 5;
+
+    const func = jest.fn();
+    const routine = new Coroutine(func, timeout);
+
+    routine.start();
+    for (let time = start; time < timeout + 3; time += 1) {
+      routine['doCallback'](time);
+    }
+    routine.stop();
+
+    expect(func).toBeCalledTimes(timeout);
+    expect(routine.startTime).toBe(start);
+    expect(routine.lastCallbackTime).toBe(timeout - 1);
+    expect(routine.callbackCount).toBe(timeout);
+    raf.mockRestore();
+  });
+
+  it('runs callback at frequency', () => {
+    const raf = mockRequestAnimationFrame();
+
+    const start = 1;
+    const total = 3;
+
+    const func = jest.fn();
+    const routine = new Coroutine(func, -1, 3);
+
+    routine.start();
+    for (let time = start; time < 10; time += 1) {
+      routine['doCallback'](time);
+    }
+    routine.stop();
+
+    expect(func).toBeCalledTimes(total);
+    expect(routine.startTime).toBe(start);
+    expect(routine.lastCallbackTime).toBe(7);
+    expect(routine.callbackCount).toBe(total);
+    raf.mockRestore();
+  });
+
+  it('runs callback until limit', () => {
+    const raf = mockRequestAnimationFrame();
+
+    const start = 0;
+    const total = 3;
+
+    const func = jest.fn();
+    const routine = new Coroutine(func, -1, -1, total);
+
+    routine.start();
+    for (let time = start; time < 10; time += 1) {
+      routine['doCallback'](time);
+    }
+    routine.stop();
+
+    expect(func).toBeCalledTimes(total);
+    expect(routine.startTime).toBe(start);
+    expect(routine.lastCallbackTime).toBe(total - 1);
+    expect(routine.callbackCount).toBe(total);
+    raf.mockRestore();
+  });
+
+  it('stops when stop is called in callback', () => {
+    const raf = mockRequestAnimationFrame();
+
+    const start = 0;
+    const total = 3;
+
+    const routine = new Coroutine(() => {
+      routine.stop();
+    });
+
+    routine.start();
+    for (let time = start; time < total; time += 1) {
+      routine['doCallback'](time);
+    }
+    routine.stop();
+
+    expect(routine.startTime).toBe(start);
+    expect(routine.lastCallbackTime).toBe(start);
+    expect(routine.callbackCount).toBe(1);
+    raf.mockRestore();
+  });
+});
--- YouTubeVideoControl/src/utils/coroutine.ts
+++ YouTubeVideoControl/src/utils/coroutine.ts
@@ -3,24 +3,33 @@ export const MSEC_PER_SEC = 1000;
 class Coroutine {
   public callback: (timestamp: number) => void;
 
-  public timeout?: number;
-  public frequency?: number;
+  public timeout: number;
+  public frequency: number;
+  public callbackLimit: number;
 
   private stopped: boolean;
 
   private _startTime: number;
   private _lastCallbackTime: number;
+  private _callbackCount: number;
 
-  constructor(callback: (timestamp: number) => void, timeout?: number, frequency?: number) {
+  constructor(
+    callback: (timestamp: number) => void,
+    timeout?: number,
+    frequency?: number,
+    callbackLimit?: number
+  ) {
     this.callback = callback;
 
-    this.timeout = timeout;
-    this.frequency = frequency;
+    this.timeout = timeout ?? -1;
+    this.frequency = frequency ?? -1;
+    this.callbackLimit = callbackLimit ?? -1;
 
     this.stopped = false;
 
-    this._startTime = 0;
-    this._lastCallbackTime = 0;
+    this._startTime = -1;
+    this._lastCallbackTime = -1;
+    this._callbackCount = 0;
   }
 
   get startTime() {
@@ -31,28 +40,39 @@ class Coroutine {
     return this._lastCallbackTime;
   }
 
-  start() {
-    requestAnimationFrame(this.doCallback.bind(this));
-    this._startTime = 0;
+  get callbackCount() {
+    return this._callbackCount;
   }
 
-  stop() {
+  start(): void {
+    requestAnimationFrame(this.doCallback.bind(this));
+  }
+
+  stop(): void {
     this.stopped = true;
   }
 
-  private doCallback(timestamp: number) {
-    if (!this._startTime) {
+  private doCallback(timestamp: number): void {
+    if (this._startTime < 0) {
       this._startTime = timestamp;
     }
 
-    if (!this.timeout || timestamp - this.startTime < this.timeout) {
-      if (!this.frequency || timestamp - this._lastCallbackTime > this.frequency) {
-        this.callback(timestamp);
-        this._lastCallbackTime = timestamp;
+    if (
+      (this.timeout < 0 || timestamp - this.startTime < this.timeout)
+      && (
+        this.frequency < 0
+        || this._lastCallbackTime < 0
+        || timestamp - this._lastCallbackTime >= this.frequency
+      )
+      && (this.callbackLimit < 0 || this._callbackCount < this.callbackLimit)
+      && !this.stopped
+    ) {
+      this.callback(timestamp);
+      this._callbackCount += 1;
+      this._lastCallbackTime = timestamp;
 
-        if (!this.stopped) {
-          requestAnimationFrame(this.doCallback.bind(this));
-        }
+      if (!this.stopped) {
+        requestAnimationFrame(this.doCallback.bind(this));
       }
     }
   }
